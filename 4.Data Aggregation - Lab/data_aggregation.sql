a1c1ba64d26bd819db44b81b9ef0780ec8ac65aa
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-09-21 21:50:46 +0300

    basic commands

--- 4.Data Aggregation - Lab/data_aggregation.sql
+++ 4.Data Aggregation - Lab/data_aggregation.sql
@@ -8,13 +8,36 @@ GROUP BY department_id
 ORDER BY department_id;
 
 -- 2.
+SELECT
+    department_id,
+    count(salary) AS employee_count
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 -- 3.
+SELECT
+    department_id,
+    sum(salary) AS total_salaries
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 -- 4.
+SELECT
+    department_id,
+    max(salary) AS max_salary
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 
 -- 5.
-
+SELECT
+    department_id,
+    AVG(salary) AS avrage_salary
+FROM employees
+GROUP BY department_id
+ORDER BY department_id;
 -- 6.
 
 
