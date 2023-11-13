2ad97a84ba5207e6b7d0deb28d4c9a7b6aa8a4b2
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-11-13 22:21:36 +0200

    Q/F objects practice

--- Django_ORM/09.Advanced Queries in Django Ex/09-exercise-orm-skeleton/orm_skeleton/main_app/managers.py
+++ Django_ORM/09.Advanced Queries in Django Ex/09-exercise-orm-skeleton/orm_skeleton/main_app/managers.py
@@ -1,47 +1,53 @@
 from decimal import Decimal
 
 from django.db import models
-from django.db.models import Q, Count, Max, Min, Avg, F
+from django.db.models import Q, Count, Avg, QuerySet, Max, Min
 
 
-class CustomManager(models.Manager):
-    def by_property_type(self, property_type: str):
+class RealEstateListingManager(models.Manager):
+    def by_property_type(self, property_type: str) -> QuerySet:
         return self.filter(property_type=property_type)
 
-    def in_price_range(self, min_price: Decimal, max_price: Decimal):
-        return self.filter(Q(price__gte=min_price) & Q(price__lte=max_price))
+    def in_price_range(self, min_price: Decimal, max_price: Decimal) -> QuerySet:
+        return self.filter(price__range=(min_price, max_price))
+        # return self.filter(price__gte=min_price, price__lte=max_price)
+        # return self.filter(Q(price__gte=min_price) & Q(price__lte=max_price))
 
-    def with_bedrooms(self, bedrooms_count: int):
+    def with_bedrooms(self, bedrooms_count: int) -> QuerySet:
         return self.filter(bedrooms=bedrooms_count)
 
-    def popular_locations(self):
-        return self.values('location').annotate(Count('id')).order_by('id')[:2]
+    def popular_locations(self) -> QuerySet:
+        return self.values('location').annotate(
+            location_count=Count('location')
+        ).order_by('-location_count', 'id')[0:2]
 
 
 # TODO
 class VideoGameManager(models.Manager):
-    def games_by_genre(self, genre: str):
+    def games_by_genre(self, genre: str) -> QuerySet:
         return self.filter(genre=genre)
 
-    def recently_released_games(self, year: int):
+    def recently_released_games(self, year: int) -> QuerySet:
         return self.filter(release_year__gte=year)
 
     def highest_rated_game(self):
-        max_rating = self.aggregate(max_rating=models.Max('rating'))['max_rating']
-        highest_rated_game = self.filter(rating=max_rating).first()
-        return highest_rated_game
+        return self.annotate(max_rating=Max('rating')).order_by('-max_rating').first()
+        # max_rating = self.aggregate(max_rating=models.Max('rating'))['max_rating']
+        # highest_rated_game = self.filter(rating=max_rating).first()
+        # return highest_rated_game
 
     def lowest_rated_game(self):
-        max_rating = self.aggregate(max_rating=models.Min('rating'))['max_rating']
-        highest_rated_game = self.filter(rating=max_rating).first()
-        return highest_rated_game
+        # slower
+        # min_rating = self.aggregate(min_rating=Min('rating'))['min_rating']
+        # self.filter(rating=min_rating).first()
+
+        # faster less queries
+        return self.annotate(min_rating=Min('rating')).order_by('min_rating').first()
+
 
     def average_rating(self):
         # return self.annotate(avg=Avg('rating')).order_by('-avg')
-        res = self.aggregate(avg=Avg('rating'))['avg']
+        res = self.aggregate(average_rating=Avg('rating'))['average_rating']
         return round(res, 1)
 
 
-class InvoiceManager(models.Manager):
-    def get_invoices_with_prefix(self, prefix):
-        return self.filter(invoice_number__contains=prefix)
--- Django_ORM/09.Advanced Queries in Django Ex/09-exercise-orm-skeleton/orm_skeleton/main_app/models.py
+++ Django_ORM/09.Advanced Queries in Django Ex/09-exercise-orm-skeleton/orm_skeleton/main_app/models.py
@@ -1,7 +1,11 @@
+from datetime import timedelta
+
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import QuerySet, Q, F
 
-from main_app.managers import InvoiceManager,CustomManager, VideoGameManager
+from main_app.managers import VideoGameManager, RealEstateListingManager
+from main_app.validators import rating_validator, release_year_validator
 
 
 # Create your models here.
@@ -21,7 +25,7 @@ class RealEstateListing(models.Model):
     bedrooms = models.PositiveIntegerField()
     location = models.CharField(max_length=100)
 
-    objects = CustomManager()
+    objects = RealEstateListingManager()
 
 
 class VideoGame(models.Model):
@@ -36,21 +40,13 @@ class VideoGame(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100, choices=GENRE_CHOICES)
     release_year = models.PositiveIntegerField(
-        validators=[
-            MinValueValidator(1930),
-            MaxValueValidator(2023)
-        ],
-        error_messages="The release year must be between 1990 and 2023"
-    )
+        validators=[release_year_validator])
+
     rating = models.DecimalField(
         max_digits=2,
         decimal_places=1,
-        validators=[
-            MinValueValidator(0.0),
-            MaxValueValidator(10.0)
-        ],
-        error_messages="The rating must be between 0.0 and 10.0"
-    )
+        validators=[rating_validator])
+
     objects = VideoGameManager()
 
     def __str__(self):
@@ -65,8 +61,24 @@ class Invoice(models.Model):
     invoice_number = models.CharField(max_length=20, unique=True)
     billing_info = models.OneToOneField(BillingInfo, on_delete=models.CASCADE)
 
-    objects = InvoiceManager()
+    @classmethod
+    def get_invoices_with_prefix(cls, prefix:str)->QuerySet:
 
+        return (
+            cls.objects.select_related(
+            'billing_info')
+                .filter(
+            invoice_number__startswith=prefix))
+
+    @classmethod
+    def get_invoices_sorted_by_number(cls):
+        return (cls.objects.select_related('billing_info')
+                .order_by('invoice_number'))
+
+    @classmethod
+    def get_invoice_with_billing_info(cls,invoice_number: str):
+        return (cls.objects.select_related('billing_info')
+                .get(invoice_number=invoice_number))
 class Technology(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -77,10 +89,15 @@ class Project(models.Model):
     description = models.TextField()
     technologies_used = models.ManyToManyField(Technology, related_name='projects')
 
+    def get_programmers_with_technologies(self) -> QuerySet:
+        return self.programmers.prefetch_related('projects__programmers')
 
 class Programmer(models.Model):
     name = models.CharField(max_length=100)
     projects = models.ManyToManyField(Project, related_name='programmers')
+
+    def get_projects_with_technologies(self):
+        return self.projects.prefetch_related('technologies_used')
 
 
 class Task(models.Model):
@@ -96,6 +113,26 @@ class Task(models.Model):
     is_completed = models.BooleanField(default=False)
     creation_date = models.DateField()
     completion_date = models.DateField()
+    @classmethod
+    def overdue_high_priority_tasks(cls)->QuerySet:
+        return cls.objects.filter(
+            Q(priority='High') & Q(is_completed=False) & Q(completion_date__gt=F('creation_date')))
+    @classmethod
+    def completed_mid_priority_tasks(cls)->QuerySet:
+        return cls.objects.filter(
+            Q(priority='Medium') & Q(is_completed=True)
+        )
+
+    @classmethod
+    def search_tasks(cls,query:str)->QuerySet:
+        return cls.objects.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
+
+    def recent_completed_tasks(self,days:int)->QuerySet:
+        return Task.objects.filter(
+            Q(is_completed=True) & Q(completion_date__gte=self.creation_date-timedelta(days=days))
+        )
 
 
 class Exercise(models.Model):
@@ -104,3 +141,25 @@ class Exercise(models.Model):
     difficulty_level = models.PositiveIntegerField()
     duration_minutes = models.PositiveIntegerField()
     repetitions = models.PositiveIntegerField()
+
+    @classmethod
+    def get_long_and_hard_exercises(cls) -> QuerySet:
+        return cls.objects.filter(
+            Q(duration_minutes__gt=30) & Q(difficulty_level__gte=10)
+        )
+    @classmethod
+    def get_short_and_easy_exercises(cls):
+        return cls.objects.filter(
+            Q(duration_minutes__lt=15) & Q(difficulty_level__lt=5)
+        )
+    @classmethod
+    def get_exercises_within_duration(cls,min_duration:int,max_duration:int):
+        return cls.objects.filter(
+            Q(duration_minutes__range=(min_duration,max_duration))
+        )
+
+    @classmethod
+    def get_exercises_with_difficulty_and_repetitions(cls,min_difficulty:int,min_repetitions:int):
+        return cls.objects.filter(
+            Q(difficulty_level__gte=min_difficulty) & Q(repetitions__gte=min_repetitions)
+        )
