3c62d3a8d31de354a7ef0b5a8cabc9c5a631a4b2
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2023-11-15 13:23:17 +0200

    exam practice 2

--- Django_ORM/00.Exam 2/orm_exam_skeleton/caller.py
+++ Django_ORM/00.Exam 2/orm_exam_skeleton/caller.py
@@ -1,9 +1,12 @@
 import os
 import django
 
+
+
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
-
+from main_app.models import Profile
 # Import your models here
 # Create and run your queries within functions
+
--- Django_ORM/00.Exam 2/orm_exam_skeleton/main_app/models.py
+++ Django_ORM/00.Exam 2/orm_exam_skeleton/main_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MinValueValidator
 
-# Create your models here.
+from main_app.managers import CustomProfileManager
+from main_app.mixins import TimeStamp
+
+
+class Profile(TimeStamp, models.Model):
+    full_name = models.CharField(max_length=100,
+     validators=[MinLengthValidator(2),])
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomProfileManager()
+    def __str__(self):
+        return self.full_name
+
+
+class Product(TimeStamp, models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+    in_stock = models.PositiveIntegerField(
+    validators=[MinValueValidator(0.01)])
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(TimeStamp, models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,related_name='orders') # related name orrders
+    products = models.ManyToManyField(Product)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.01)])
+
+    is_completed = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f"Order #{self.id} - Total Price: {self.total_price} - Completed: {self.is_completed}"
